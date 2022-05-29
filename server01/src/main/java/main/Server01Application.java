de2b11551e3f7194cae807113fd3fe321c05e13b
dlwlstks96 <72801465+dlwlstks96@users.noreply.github.com>
2022-05-29 15:55:53 +0900

    데이터 조회, 삭제 기능 구현

--- server01/src/main/java/main/Server01Application.java
+++ server01/src/main/java/main/Server01Application.java
@@ -20,18 +20,21 @@ public class Server01Application {
 		AnnotationConfigApplicationContext ctx =
 				new AnnotationConfigApplicationContext(AppCtx.class);
 
+		SpringApplication.run(Server01Application.class, args);
+
 		ticketDao = ctx.getBean(TicketDao.class);
 
 		insertTicketInfo();
-		SpringApplication.run(Server01Application.class, args);
+		//selectTicketInfo();
+		//deleteTicketInfo();
 
 		ctx.close();
 
 	}
 
 	private static void insertTicketInfo() throws IOException {
-		System.out.println("-----inset Ticket info");
-		System.out.println("등록할 티켓 정보를 입력하세요: (name birth seatNumber)");
+		System.out.println("-----insert Ticket info");
+		System.out.println("예매할 티켓 정보를 입력하세요: (name birth seatNumber)");
 
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String userInput = reader.readLine();
@@ -43,6 +46,47 @@ public class Server01Application {
 		Ticket ticket = new Ticket(name, birth, seatNumber);
 
 		ticketDao.insert(ticket);
+
+		System.out.println("예매 완료");
+	}
+
+	private static void selectTicketInfo() throws IOException {
+		System.out.println("-----select Ticket info");
+		System.out.println("조회할 좌석 정보를 입력하세요: (name birth seatNumber)");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String userInput = reader.readLine();
+		String[] ticketInfo = userInput.split(" ");
+		String name = ticketInfo[0];
+		String birth = ticketInfo[1];
+		String seatNumber = ticketInfo[2];
+
+		//Ticket ticket = new Ticket(name, birth, seatNumber);
+
+		Ticket ticket = ticketDao.selectBySeatNumber(name, birth, seatNumber);
+
+		System.out.println("조회하신 예매 내역입니다.");
+		System.out.println("이름: " + ticket.getName() + " 생년월일: " + ticket.getBirth() +
+				" 좌석 번호: " + ticket.getSeatNumber() + " 예매 번호: " + ticket.getTicketID());
+
+	}
+
+	private static void deleteTicketInfo() throws IOException {
+		System.out.println("-----delete Ticket info");
+		System.out.println("삭제할 좌석 정보를 입력하세요: (name birth seatNumber)");
+
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String userInput = reader.readLine();
+		String[] ticketInfo = userInput.split(" ");
+		String name = ticketInfo[0];
+		String birth = ticketInfo[1];
+		String seatNumber = ticketInfo[2];
+
+		Ticket ticket = new Ticket(name, birth, seatNumber);
+
+		ticketDao.delete(ticket);
+
+		System.out.println("삭제 완료");
 	}
 
 }
